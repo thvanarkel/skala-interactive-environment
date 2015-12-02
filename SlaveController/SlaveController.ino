f9d75ce45463c413f3d15512a6e7f34f81eb4e14
Claudio <lazocb@gmail.com>
2015-12-02 17:33:56 +0100

    Smarter Master
    The master now keeps track of the servo states. the slave just gets a
    command (servo x, 30 degrees) and does it.
    There is still a timing issue that needs to be solved.

--- SlaveController/SlaveController.ino
+++ SlaveController/SlaveController.ino
@@ -2,52 +2,31 @@
 #include <Servo.h>
 
 //slave 0:
-//const byte SLAVE_ID = 0;
-//const byte  NUM_SERVOS = 4;
-//const byte  ports[NUM_SERVOS] = {2, 3, 4, 5};
+
+const byte SLAVE_ID = 0;
+const byte  NUM_SERVOS = 4;
+const byte  ports[NUM_SERVOS] = {2, 3, 4, 5};
 
 //slave 1:
+/*
 const byte SLAVE_ID = 1;
 const byte  NUM_SERVOS = 2;
 const byte  ports[NUM_SERVOS] = {2, 3};
-
+*/
 // slave 2:
-//const byte SLAVE_ID = 2;
-//const byte NUM_SERVOS = 5;
-//const byte ports[NUM_SERVOS] = {2, 3, 5, 6, 7};
-
-const int RUSTLE_DELAY = 400;
-const int RUSTLE_ANGLE = 50;
-
-const int BUZZING_DELAY = 50;
-const int BUZZING_ANGLE = 10;
-
-const int stateLadderNormal = 0;
-const int stateLadderRustling = 1;
-const int stateLadderCascading = 2;
-const int stateLadderBuzzing = 4;
-
-struct aServo {
-  Servo theServo;
-  int state;
-  int angle;
-  bool didTurn;
-  unsigned long previousMovement;
-};
-
-struct aServo servos[NUM_SERVOS];
+/*
+const byte SLAVE_ID = 2;
+const byte NUM_SERVOS = 5;
+const byte ports[NUM_SERVOS] = {2, 3, 5, 6, 7};
+*/
+Servo servos[NUM_SERVOS];
 
 void setup() {
   // initiate the servos:
   for (int i = 0; i < NUM_SERVOS; i++) {
-    struct aServo servo;
-    servo.theServo.attach(ports[i]);
-    servo.state = stateLadderNormal;
-    servo.angle = 0;
-    servo.theServo.write(0);
-    servo.previousMovement = millis();
-    servo.didTurn = false;
-    servos[i] = servo;
+    Servo theServo;
+    theServo.attach(ports[i]);
+    servos[i] = theServo;
   }
 
   // Start the I2C comms as a slave
@@ -58,108 +37,38 @@ void setup() {
 }
 
 void receiveMessage(int numBytes) {
-  Serial.println("Did receive");
+  Serial.println("\nReceived message:");
   byte message[numBytes];
   for ( int i = 0; i < numBytes; i++) {
     message[i] = Wire.read();
   }
-  byte index = message[0];
-  Serial.print(index);
-  Serial.print(", ");
-  byte state = message[1];
-  Serial.println(state);
-  setState(index, state);
+  byte servoId = message[0];
+  byte angle = message[1];
 
-}
+   Serial.print("Local Servo ID: ");
+   Serial.print(servoId);
+   Serial.print(" must turn to ");
+   Serial.print(angle);
+   Serial.print(" degrees");
 
-void updateServos() {
+  byte servoArrayId;
   for (int i = 0; i < NUM_SERVOS; i++) {
-    struct aServo servo = servos[i];
-    switch (servo.state) {
-      case stateLadderNormal:
-        if (servo.angle > 90) {
-          servo.angle = 180;
-        } else {
-          servo.angle = 0;
-        }
-        servo.theServo.write(servo.angle);
-        break;
-
-      case stateLadderRustling:
-        //        Serial.println("State rustling");
-        if (millis() - servo.previousMovement > RUSTLE_DELAY) {
-          if (servo.angle > 90) {
-            if (servo.angle == 180) {
-              servo.angle = 180 - RUSTLE_ANGLE;
-            } else {
-              servo.angle = 180;
-            }
-          } else {
-            if (servo.angle == 0) {
-              servo.angle = RUSTLE_ANGLE; 
-            } else {
-              servo.angle = 0;
-            }
-          }
-          servo.previousMovement = millis();
-          servo.theServo.write(servo.angle);
-        }
-        break;
-
-      case stateLadderCascading:
-        //Serial.println("State cascading");
-        if (!servo.didTurn) {
-          if (servo.angle > 90) {
-            servo.angle = 0;
-          } else {
-            servo.angle = 180;
-          }
-          servo.didTurn = true;
-          servo.theServo.write(servo.angle);  
-        }
-        
-        break;
-
-      case stateLadderBuzzing:
-        //        Serial.println("State buzzing");
-        if (millis() - servo.previousMovement > BUZZING_DELAY) {
-          if (servo.angle > 90) {
-            if (servo.angle == 180) {
-              servo.angle = 180 - BUZZING_ANGLE;
-            } else {
-              servo.angle = 180;
-            }
-          } else {
-            if (servo.angle == 0) {
-              servo.angle = BUZZING_ANGLE; 
-            } else {
-              servo.angle = 0;
-            }
-          }
-          servo.previousMovement = millis();
-          servo.theServo.write(servo.angle);
-        }
-        break;
-    }
-    servos[i] = servo;
-  }
-}
-
-void setState(int index, int state) {
-  int realIndex = 0;
-  for (int i = 0; i < NUM_SERVOS; i++) {
-    if (index == ports[i]) {
-      realIndex = i;
-      break;
+    if (ports[i] == servoId) {
+      servoArrayId = i;
     }
   }
-  struct aServo servo = servos[realIndex];
-  servo.state = state;
-  servo.didTurn = false;
-  servos[realIndex] = servo;
+    Serial.print("\nWriting to servo # ");
+    Serial.print(servoArrayId);
+    Serial.print(". Angle: ");
+    Serial.print(angle);
+
+  Servo theServo = servos[servoArrayId];
+  theServo.write(angle);
 }
 
 void loop() {
-  updateServos();
+ // servos[0].write(random(0,45));
+//  delay(1000);
+  delay(1);
 }
 
